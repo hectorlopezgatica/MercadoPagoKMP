779658d6c4117ef7d3e7084c58283c2b5ab8761b
Hector <Hector.LopezGatica@paramount.com>
2023-08-15 11:34:31 -0400

    Initial commit

---
+++ android/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("org.jetbrains.compose")
+    id("com.android.application")
+    kotlin("android")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":common"))
+    implementation("androidx.activity:activity-compose:1.6.1")
+}
+
+android {
+    compileSdk = 34
+    buildFeatures {
+        compose = true
+    }
+    defaultConfig {
+        applicationId = "com.example.android"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0-SNAPSHOT"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+tasks.register("BuildAndRun") {
+    doFirst {
+        exec {
+            workingDir(projectDir.parentFile)
+            commandLine("./gradlew", "android:build")
+            commandLine("./gradlew", "android:installDebug")
+        }
+    }
+}
---
+++ common/build.gradle.kts
@@ -0,0 +1,108 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    id("com.android.library")
+    kotlin("native.cocoapods")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+@OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+kotlin {
+    android()
+    jvm("desktop")
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    ios()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        version = "1.0"
+        ios.deploymentTarget = "14.1"
+        podfile = project.file("../iosApp/Podfile")
+        framework {
+            baseName = "common"
+            isStatic = true
+        }
+        extraSpecAttributes["resources"] = "['src/commonMain/resources/**', 'src/iosMain/resources/**']"
+    }
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(compose.runtime)
+                api(compose.ui)
+                api(compose.foundation)
+                api(compose.materialIconsExtended)
+                api(compose.material3)
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val androidMain by getting {
+            dependencies {
+                api("androidx.appcompat:appcompat:1.6.0")
+                api("androidx.core:core-ktx:1.9.0")
+                api(compose.preview)
+            }
+        }
+
+        val androidTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+            }
+        }
+
+        val desktopMain by getting {
+            dependencies {
+                api(compose.preview)
+            }
+        }
+
+        val desktopTest by getting
+
+        val jsMain by getting {
+            dependencies {
+                api(compose.web.core)
+            }
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by getting {
+            dependencies {
+
+            }
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+    }
+
+    explicitApi()
+}
+
+android {
+    compileSdk = 34
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
